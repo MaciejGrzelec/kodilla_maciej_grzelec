88613157c61aaf8e9e4b359a5604faeb5bf3a10c
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-10-11 13:14:28 +0200

    First attempt to Heroku

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -4,32 +4,48 @@ import com.crud.tasks.domain.TrelloBoardDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-
+@Component
 public class TrelloClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${trello.api.endpoint.prod}")
+    @Value("trello.api.endpoint.prod")
     private String trelloApiEndpoint;
 
-    @Value("${trello.app.key}")
+    @Value("trello.app.key")
     private String trelloAppKey;
 
-    @Value("${trello.app.token}")
-    private String trelLoToken;
+    @Value("trello.app.token")
+    private String trelloToken;
 
-    public void getTrelloBoards() {
+    //@Value("trello.username")
+    //private String trelloUsername;
 
-        //TrelloBoardDto[] boardsResponse = restTemplate.getForObject(trelloApiEndpoint+"members/maciejgrzelec/boards"+"?key=" +trelloAppKey +"&token=" + trelLoToken, TrelloBoardDto[].class);
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "members/maciejgrzelec/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelLoToken)
-                .queryParam("fields", "name,id").build().encode().toUri();
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        //TrelloBoardDto[] boardsResponse = restTemplate.getForObject(trelloApiEndpoint+"members/maciejgrzelec/boards"+"?key=" +trelloAppKey +"&token=" + trelloToken, TrelloBoardDto[].class);
+        URI url = getUri();
 
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+
+        if (boardsResponse != null) {
+            return Arrays.asList(boardsResponse);
+        }
+        return new ArrayList<>();
+    }
+
+    private URI getUri() {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "members/maciejgrzelec/boards")
+                    .queryParam("key", trelloAppKey)
+                    .queryParam("token", trelloToken)
+                    .queryParam("fields", "name,id").build().encode().toUri();
     }
 }

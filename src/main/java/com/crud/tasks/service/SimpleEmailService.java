e400625fa151c21b903ed07ce8496f273f5ebce8
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-10-15 13:20:29 +0200

    a

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class SimpleEmailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void send(final Mail mail) {
+        LOGGER.info("Starting email preparation...");
+        try {
+            javaMailSender.send(createMailMessage(mail));
+        LOGGER.info("Email has been sent.");
+    } catch (MailException e) {
+        LOGGER.error("Failed to process mail sending: ", e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        if (mail.getToCC()!=null) {
+        mailMessage.setCc(mail.getToCC());
+        } return mailMessage;
+    }
+}

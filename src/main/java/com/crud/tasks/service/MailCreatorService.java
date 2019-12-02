342f628cd0d70c5dea2ce27401009bdaa3bee27f
maciejgrzelec86 <maciej.kodilla.grzelec@gmail.com>
2019-12-03 00:53:02 +0100

    new frontend heroku

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
     @Autowired
@@ -17,6 +20,12 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> funcionality = new ArrayList<>();
+        funcionality.add("You can manage your tasks");
+        funcionality.add("Provides connection with Trello Account");
+        funcionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
@@ -25,6 +34,10 @@ public class MailCreatorService {
         context.setVariable("goodbye_message", "bye bye");
         context.setVariable("preview", "preview");
         context.setVariable("company", "Kodilla");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", funcionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
